c92ef7227fb69a9acb82c3c1682138a24788a20c
Ashokmani1 <46559785+ashokmani1@users.noreply.github.com>
2024-11-09 23:07:46 +0530

    Added Splash Screen and Navigation Lib

--- app/src/main/java/com/sample/snoozeloo/MainActivity.kt
+++ app/src/main/java/com/sample/snoozeloo/MainActivity.kt
@@ -3,47 +3,86 @@ package com.sample.snoozeloo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.sample.snoozeloo.ui.theme.SnoozelooTheme
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity()
 {
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
+        showSplashScreen()
+
         setContent {
+
             SnoozelooTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android", modifier = Modifier.padding(innerPadding)
-                    )
+
+                Surface(color = Color.Blue, modifier = Modifier.fillMaxSize()) {
+
+                    Navigation()
                 }
+            }
+        }
+    }
+
+    private fun showSplashScreen()
+    {
+        val splashScreen = installSplashScreen()
+
+        var keepSplashScreen = true
+
+        // Set the condition to keep the splash screen on screen
+        splashScreen.setKeepOnScreenCondition { keepSplashScreen }
+
+        lifecycleScope.launch {
+
+            delay(2000) // Keeps splash screen for 2 seconds
+            keepSplashScreen = false
+        }
+    }
+}
+
+
+
+
+@Composable
+fun Navigation() {
+
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "main_screen") {
+
+        composable("main_screen") {
+
+            Box(modifier = Modifier.fillMaxSize().background(MaterialTheme.colorScheme.background)) {
+                Text(
+                    text = "Welcome to splash screen",
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp,
+                    color = MaterialTheme.colorScheme.onBackground,
+                    modifier = Modifier.align(Alignment.Center)
+                )
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier)
-{
-    Text(
-        text = "Hello $name!", modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview()
-{
-    SnoozelooTheme {
-        Greeting("Android")
-    }
-}
--- app/src/main/java/com/sample/snoozeloo/ui/theme/Theme.kt
+++ app/src/main/java/com/sample/snoozeloo/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.sample.snoozeloo.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -16,17 +15,16 @@ private val DarkColorScheme = darkColorScheme(
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40, secondary = PurpleGrey40, tertiary = Pink40
+    primary = Primary,
+    secondary = Secondary,
+    tertiary = Tertiary,
+    background = Background,
+    surface = Surface,
+    onPrimary = OnPrimary,
+    onSecondary = OnSecondary,
+    onBackground = OnBackground,
+    onSurface = OnSurface,
 
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
 )
 
 @Composable
