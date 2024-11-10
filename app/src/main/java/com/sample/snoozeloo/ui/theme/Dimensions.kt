d085471693642a28deb05f089156ac728377ef58
Ashokmani1 <46559785+ashokmani1@users.noreply.github.com>
2024-11-10 12:46:33 +0530

    Alarm List Ui changes

---
+++ app/src/main/java/com/sample/snoozeloo/ui/theme/Dimensions.kt
@@ -0,0 +1,15 @@
+package com.sample.snoozeloo.ui.theme
+
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+val DIMEN_15_dp = 15.dp
+val DIMEN_10_dp = 10.dp
+val DIMEN_5_dp = 5.dp
+val DIME_6_dp = 6.dp
+val DIME_7_dp = 7.dp
+val DIMEN_11_dp = 11.dp
+val DIME_3_dp = 3.dp
+
+
+val DIMEN_24_SP = 24.sp
